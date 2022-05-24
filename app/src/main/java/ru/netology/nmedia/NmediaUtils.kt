64ca3d22b6691d23e9797d79512808fc9a53540c
maks-x <100487620+maks-x@users.noreply.github.com>
2022-05-24 14:04:55 +0300

    CRUD: remove redundant import

--- app/src/main/java/ru/netology/nmedia/NmediaUtils.kt
+++ app/src/main/java/ru/netology/nmedia/NmediaUtils.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import ru.netology.nmedia.databinding.PostsFeedBinding
-import kotlin.math.acos
 
 internal fun View.hideKeyboard() {
     val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
