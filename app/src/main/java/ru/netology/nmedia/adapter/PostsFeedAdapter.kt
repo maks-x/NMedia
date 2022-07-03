cdaaaf5ab4e7e814871025eef9675da9f80f70d0
maks-x <100487620+maks-x@users.noreply.github.com>
2022-07-03 16:37:30 +0300

    Util extension fun PostBinding.setBasicListeners(...) added and used in ViewHolder with new private property post:Post from adapter.currentList.first()

--- app/src/main/java/ru/netology/nmedia/adapter/PostsFeedAdapter.kt
+++ app/src/main/java/ru/netology/nmedia/adapter/PostsFeedAdapter.kt
@@ -2,14 +2,13 @@ package ru.netology.nmedia.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.PopupMenu
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import ru.netology.nmedia.R
 import ru.netology.nmedia.databinding.PostBinding
 import ru.netology.nmedia.objects.Post
 import ru.netology.nmedia.utils.fillWithPost
+import ru.netology.nmedia.utils.setBasicListeners
 import ru.netology.nmedia.viewModel.PostInteractionListener
 
 internal class PostsFeedAdapter(
@@ -21,6 +20,7 @@ internal class PostsFeedAdapter(
         val postBinding = PostBinding.inflate(
             inflater, parent, false
         )
+
         return ViewHolder(postBinding, interactionListener)
     }
 
@@ -34,36 +34,14 @@ internal class PostsFeedAdapter(
         listener: PostInteractionListener
     ) : RecyclerView.ViewHolder(postBinding.root) {
 
-        private lateinit var post: Post
-
-        private val popupMenu by lazy {
-            PopupMenu(itemView.context, postBinding.postsOptions).apply {
-                inflate(R.menu.options_post)
-                setOnMenuItemClickListener { item ->
-                    when (item.itemId) {
-                        R.id.popupRemove -> {
-                            listener.onRemoveClick(post.id)
-                            true
-                        }
-                        R.id.popupEdit -> {
-                            listener.onEditClick(post)
-                            true
-                        }
-                        else -> false
-                    }
-                }
-            }
-        }
+//        необходимо инициализировать значение post для использования
+//        в стороннем коде (setBasicListeners)
+        private var post = Post()
 
         init {
             with(postBinding) {
-                likes.setOnClickListener { listener.onLikeClick(post.id) }
-                share.setOnClickListener { listener.onShareClick(post) }
-                postsOptions.setOnClickListener { popupMenu.show() }
-                videoPlay.setOnClickListener {
-                    post.videoLink?.let { listener.onVideoLinkClick(it) }
-                }
-                videoPreview.setOnClickListener { videoPlay.performClick() }
+                setBasicListeners(post, listener)
+                postNavigateArea.setOnClickListener { listener.onPostNavigateAreaClick(post) }
             }
         }
 
